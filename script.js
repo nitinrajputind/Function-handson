c54bdc41eb2874e6cab2f796e73e58a21bd76950
nitinrajputind <nitinrajput971625@gmail.com>
2023-06-07 18:16:23 +0530

    Completed function handson

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 // Question :1
-function question(){
-    console.log("This is Zero peremeter Function");
+function question() {
+  console.log("This is Zero peremeter Function");
 }
 question();
 
 // Question :2
-function question2(a,b){
-    let sum = a+b;
-    console.log(sum);
+function question2(a, b) {
+  let sum = a + b;
+  console.log(sum);
 }
-question2(3,4);
+question2(3, 4);
 
 // Question :3
 
-const arrow=()=>{
-    console.log("This is arrow function ");
-}
+const arrow = () => {
+  console.log("This is arrow function ");
+};
 arrow();
 
 // question :4
-// Print output: 
+// Print output:
 var x = 21;
 var girl = function () {
-    console.log(x);
-    var x = 20;
+  console.log(x);
+  var x = 20;
 };
-girl ();
+girl();
 
-// Question :5 
+// Question :5
 var x = 21;
-girl ();
-console.log(x)
+girl();
+console.log(x);
 function girl() {
-    console.log(x);
-    var x = 20;
-};
+  console.log(x);
+  var x = 20;
+}
 
 // Question :6
 var x = 21;
@@ -42,41 +42,61 @@ a();
 b();
 
 function a() {
-    
-   x = 20;
+  x = 20;
   console.log(x);
-};
- function b() {
-    
-    x = 40;
-   console.log(x);
-};
-
-//question :7
-const fact=(num)=>{
-    ans=1
-    if(num>0){
-        for (let i=1; i<=num;i++){
-            ans=ans*i
-        }
-        console.log(ans)
-    }
-    else{
-        console.log("enter a postive no")
-    }
+}
+function b() {
+  x = 40;
+  console.log(x);
 }
 
-fact(5)
+//question :7
+const fact = (num) => {
+  ans = 1;
+  if (num > 0) {
+    for (let i = 1; i <= num; i++) {
+      ans = ans * i;
+    }
+    console.log(ans);
+  } else {
+    console.log("enter a postive no");
+  }
+};
+
+fact(5);
 
 /* =============================================     Day :- 2    ======================================================== */
 
 // question: 1
-function FindSum(a, b){
-    return a + b;
+function FindSum(a, b) {
+  return a + b;
 }
 
-function DisplayData(data, batch){
-    console.log(`i am from ${data} and My batch is EA${batch}`)
+function DisplayData(data, batch) {
+  console.log(`i am from ${data} and My batch is EA${batch}`);
 }
 
 DisplayData("PrepBytes", FindSum(10, 13));
+
+// Question :2
+Abc();
+const Abc = function () {
+  let value = 20;
+  console.log(value);
+};
+
+// Question :3
+var a = 10;
+(function (){
+    console.log(a);
+    var a = 20;
+})();
+
+// Question :4
+const greet =  function(name){
+    return function(m){
+        console.log(`Hi!! ${name}, ${m}`);
+    }
+}
+const greet_message = greet('EAC-01');
+greet_message("Welcome To PrepBytes")
